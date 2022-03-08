c3521c6f25948c5dc8cc5e358b79ff24a4de49bb
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-03-08 16:48:09 +0300

    Components added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import NavBar from './Components/NavBar';
+import SideBar from './Components/SideBar';
+import MainContent from './Components/MainContent';
 
 function App() {
   return (
     <>
-      <h2>hello</h2>
+      <NavBar />
+      <div className="wrapped">
+      <SideBar />
+      <MainContent />
+        
+      </div>
     </>
   );
 }
