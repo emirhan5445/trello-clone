364256a12b23eb4903aa43160239e05a69f01e90
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-03-08 16:48:39 +0300

    MainContent file added.

---
+++ src/Components/MainContent.js
@@ -0,0 +1,12 @@
+import './MainContent.css';
+
+function MainContent() {
+  return (
+    <>
+        <div className='right-content'>
+        </div>
+    </>
+  );
+}
+
+export default MainContent;
