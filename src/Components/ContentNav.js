8b09b7251bfcf0204ae594f724627d3153a4878a
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-03-10 23:00:37 +0300

    Updated.

--- src/Components/ContentNav.js
+++ src/Components/ContentNav.js
@@ -5,13 +5,21 @@ import FilterListIcon from '@mui/icons-material/FilterList';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
 import AddList from '../Components/AddList';
+import { useContext } from 'react';
+import { AppContext } from '../Contexts/AppContext';
+
 /* import { useContext } from 'react';
 import { AppContext } from '../Contexts/AppContext';   */
 
 function ContentNav() {
+  const { isTrue } = useContext(AppContext);  
+
   return (
     <>
-        <div className='right-content' id='width'>
+    {
+      isTrue ?
+      
+        <div className='right-content' style={{ width: '100vw', height: '93vh' }} id='width'>
           <ul className='left-list'>
             <li><span><BarChartOutlinedIcon /> Board <i className="fa fa-angle-down board-angle"></i> </span></li>
             <li><span>Untitled</span></li>
@@ -24,7 +32,7 @@ function ContentNav() {
             <li><span><AccountCircleOutlinedIcon /></span></li>
             <li><span><i className="fa-solid fa-user-plus"></i> Invite</span></li>
           </ul>
-            
+
           <ul className='right-list'>
             <li><span><i className="fa-solid fa-rocket"></i> Power-ups</span></li>
             <li><span><ElectricBoltIcon /> Automation</span></li>
@@ -32,9 +40,31 @@ function ContentNav() {
             <li><span><FilterListIcon /> Filter</span></li>
             <li><span><i className="fa-solid fa-ellipsis"></i> Show menu</span></li>
           </ul>
-        </div>
+        </div> :
+        <div className='right-content' style={{ width: '82vw', height: '93vh' }} id='width'>
+        <ul className='left-list'>
+          <li><span><BarChartOutlinedIcon /> Board <i className="fa fa-angle-down board-angle"></i> </span></li>
+          <li><span>Untitled</span></li>
+          <li><span><i className="fa-regular fa-star"></i></span></li>
+          <li className='cizgi'></li>
+          <li><span>Name</span></li>
+          <li className='cizgi'></li>
+          <li><span><LockIcon /> Workspace visible</span></li>
+          <li className='cizgi'></li>
+          <li><span><AccountCircleOutlinedIcon /></span></li>
+          <li><span><i className="fa-solid fa-user-plus"></i> Invite</span></li>
+        </ul>
 
-        <AddList />
+        <ul className='right-list'>
+          <li><span><i className="fa-solid fa-rocket"></i> Power-ups</span></li>
+          <li><span><ElectricBoltIcon /> Automation</span></li>
+          <li className='cizgi'></li>
+          <li><span><FilterListIcon /> Filter</span></li>
+          <li><span><i className="fa-solid fa-ellipsis"></i> Show menu</span></li>
+        </ul>
+      </div>
+    }
+
     </>
   );
 }
