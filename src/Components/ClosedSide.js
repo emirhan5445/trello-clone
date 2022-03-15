81db03c49cff15d1ff10a3c45883dc378d4c9deb
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-03-15 12:39:14 +0300

    Hide and show buttons are done.

--- src/Components/ClosedSide.js
+++ src/Components/ClosedSide.js
@@ -4,19 +4,13 @@ import { useContext } from 'react';
 import { AppContext } from '../Contexts/AppContext';
 
 function ClosedSide() {
-  const { setIsTrue, isTrue } = useContext(AppContext);
-  console.log(isTrue);
+  const { setIsTrue } = useContext(AppContext);
+
   return (
     <>
-      <div /* onClick={
-        isTrue ?
-        setIsTrue(false) :
-        null
-      } */ className="closed-side">
+      <div onClick={() => setIsTrue(currentShow => !currentShow)} className="closed-side">
         <ChevronRightIcon sx={{ position: 'absolute', top: "10px", left:"3px", bgcolor: '#3887B4', borderRadius: "50px", cursor: "pointer", border: "1px solid rgba(175, 175, 175,0.6)", fontSize:"25px" , padding:"3px"}} />
-
       </div>
-
     </>
   );
 }
